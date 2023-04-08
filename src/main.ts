4edc955fbe7777eb2d144c2e88f365a002cc1ab3
Ruvian S <12111730+matricce@users.noreply.github.com>
2023-04-08 11:36:29 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import bot from './bot';
+import { Delivery, deliveryEvents } from './delivery';
+import { development } from './utils/start';
+
+bot.on('channel_post').on('msg', async ctx => Delivery(bot, ctx));
+
+deliveryEvents(bot);
+
+process.env.NODE_ENV === 'development' && development(bot);
+
+export {};
